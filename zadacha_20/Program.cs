eb57541ac42328707242bce1d21dcd92c9f4fa71
Larina <Losiknor@gmail.com>
2022-02-14 15:34:18 +0700

    Решение задачи 20

--- zadacha_20/Program.cs
+++ zadacha_20/Program.cs
@@ -1,2 +1,24 @@
 ﻿// № 20 Задать номер четверти, показать диапазоны для возможных координат
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Задайте номер черверти от 1 до 4: ");
+int number = int.Parse(Console.ReadLine());
+if (number == 1)
+    Console.WriteLine("Диапазоны для возможных координат: x>0 y>0");
+else
+{
+    if (number == 2)
+    Console.WriteLine("Диапазоны для возможных координат: x<0 y>0");
+    else
+    {
+        if (number == 3)
+           Console.WriteLine("Диапазоны для возможных координат: x<0 y<0");
+        else
+        {
+            if (number == 4)
+               Console.WriteLine("Диапазоны для возможных координат: x>0 y<0");
+            else Console.WriteLine("Вы неправильно здали номер четверти, попробуйте ещё раз");
+        }
+    }
+}
+
+
+
