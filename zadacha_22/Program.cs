7a55637c5aafd2e6e5dfd682ef50f3cc62e89125
Larina <Losiknor@gmail.com>
2022-02-14 17:09:08 +0700

    Решение задачи 22

--- zadacha_22/Program.cs
+++ zadacha_22/Program.cs
@@ -1,2 +1,8 @@
 ﻿// № 22 Найти расстояние между точками в пространстве 2D/3D
-Console.WriteLine("Hello, World!");
+int x1 = new Random().Next(-100, 100);
+int y1 = new Random().Next(-100, 100);
+int x2 = new Random().Next(-100, 100);
+int y2 = new Random().Next(-100, 100);
+Console.WriteLine("Заданы две точки с координатами (" + x1 + ";" + y1 + ") и (" + x2 + ";" + y2 + ")");
+Console.WriteLine("Расстояние между данными точками:  " + Math.Sqrt(Math.Pow(x1-x2, 2) + Math.Pow(y1-y2, 2)));
+// про 3D подумаю позже
