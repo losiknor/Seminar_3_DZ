74aa1d036fb4b192fbb06f90895e61d632bcb4cd
Larina <Losiknor@gmail.com>
2022-02-14 14:16:24 +0700

    Решение задачи 24

--- zadacha_24/Program.cs
+++ zadacha_24/Program.cs
@@ -1,2 +1,7 @@
 ﻿// № 24 Найти кубы чисел от 1 до N
-Console.WriteLine("Hello, World!");
+// int number = new Random().Next(1, 10);
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Кубы чисел от 1 до " + number + " :");
+for (int i = 1; i <= number; i++)
+    Console.WriteLine(Math.Pow(i, 3));
