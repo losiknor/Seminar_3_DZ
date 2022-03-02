fa80f6b3ca58ab6c0f32228473b3c8347333a658
Larina <Losiknor@gmail.com>
2022-03-02 14:07:28 +0700

    Плюс второй вариант решения через массив

--- zadacha_18/Program.cs
+++ zadacha_18/Program.cs
@@ -38,3 +38,33 @@ else
         }
     }
 }
+// 2 Вариант решения через массив
+bool[] all = {true, false};
+
+bool result = true;
+
+for(int i = 0; i <all.Length; i++)
+{
+    for(int j = 0; j <all.Length; j++)
+    {
+        if( !(all[i] || all[j]) == (!all[i] && !all[j]))
+        {
+            Console.WriteLine($"Для набора x = {all[i]}, y = {all[j]} выражение истинно");
+        }
+        else
+        {
+            Console.WriteLine($"Для набора x = {all[i]}, y = {all[j]} выражение ложно");
+            result = false;
+            break;
+        }
+
+    }
+}
+
+if(result)
+{
+    Console.WriteLine($"Доказано что утверждение истинно");
+} else
+{
+    Console.WriteLine($"Доказано что утверждение ложно");
+}
